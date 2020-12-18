019ffaad7253bc110fe32eacdf1e6d56f0021d1f
Adam Nowland <acnowland@gmail.com>
2020-12-18 18:49:25 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    return programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,8 +84,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Unix"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash
 
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,6 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +136,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 
 end
